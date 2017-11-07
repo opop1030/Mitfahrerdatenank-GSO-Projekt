4c837ff62552d0ef0f02310f32bf3333020641cf
Marcel Doering <privat@marcel-doering.de>
2017-11-07 14:12:15 +0100

    Add login form

--- App/Views/HomeView.php
+++ App/Views/HomeView.php
@@ -3,12 +3,53 @@ namespace App\Views;
 
 class HomeView
 {
-    public function __construct() {
+    public function __construct() 
+    {
+
     }
 
-    public function start()
+    public function render()
     {
-        echo "<h1>Hello World</h1>";
+        echo 
+        '<h2>Anmelden</h2>
+        <form id="login">
+            <div class="form-group row">
+                <label for="inputEmail" class="col-sm-2 col-form-label">User</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputEmail" placeholder="Benutzername">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Passwort</label>
+                <div class="col-sm-10">
+                <input type="password" class="form-control" id="inputPassword" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Anmelden</button>
+                </div>
+            </div>        
+        </form>
+        <form id="register">
+        <div class="form-group row">
+            <label for="inputEmail" class="col-sm-2 col-form-label">User</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputEmail" placeholder="Benutzername">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputPassword" class="col-sm-2 col-form-label">Passwort</label>
+            <div class="col-sm-10">
+            <input type="password" class="form-control" id="inputPassword" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Anmelden</button>
+            </div>
+        </div>        
+    </form>';
     }
 }
 ?>
