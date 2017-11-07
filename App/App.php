c0dd83f2b7da03b7789137b225f4ceaa7c7fb5dc
Marcel Doering <privat@marcel-doering.de>
2017-11-07 12:25:54 +0100

    Add the basic framework

---
+++ App/App.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+
+class App
+{
+    public function __construct() {
+    }
+
+    public function start()
+    {
+        echo "<h1>Hello World</h1>";
+    }
+}
+?>
---
+++ App/Views/HomeView.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Views;
+
+class HomeView
+{
+    public function __construct() {
+    }
+
+    public function start()
+    {
+        echo "<h1>Hello World</h1>";
+    }
+}
+?>
